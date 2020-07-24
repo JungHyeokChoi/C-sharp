5188edf6a5b4c8b208902fbac772ec502512e105
JungHyeokChoi <page1114@gmail.com>
2020-07-24 12:52:42 +0900

    Test OpenWeatherMap

---
+++ .NET_core/OpenWeatherMap_Test/OpenWeatherMap_Test/OpenWeatheraMap.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace OpenWeatherMap_Test
+{
+    //Reference : https://openweathermap.org/weather-data
+    public class Coord
+    {
+        //longitude
+        public double lon { get; set; }
+        //Latitude
+        public double lat { get; set; }
+    }
+    public class Weather
+    {
+        //Reference : https://openweathermap.org/weather-conditions
+        //Weather condition ID
+        public int id { get; set; }
+        //Group of weather parameters (Rain, Snow, Extreme etc.)
+        public string main { get; set; }
+        //Weather condition within the group
+        public string description { get; set; }
+        //Weather icon ID
+        public string icon { get; set; }
+    }
+    public class Main
+    {
+        //Temperature(Standare:Kelvin, Metric:Celsius, Imperial:Fahrenheit)
+        public double temp { get; set; }
+        //Sensible Temperature
+        public double sensible_temp { get; set; }
+        //Minimum temperature at the moment(Standare:Kelvin, Metric:Celsius, Imperial:Fahrenheit)
+        public double temp_min { get; set; }
+        //Maximum temperature at the moment(Standare:Kelvin, Metric:Celsius, Imperial:Fahrenheit)
+        public double temp_max { get; set; }
+        //Pressure(hPa)
+        public int pressure { get; set; }
+        //Humidity(%)
+        public int humidity { get; set; }
+    }
+    public class Wind
+    {
+        //Wind speed(m/s)
+        public double speed { get; set; }
+        //Wind direction(degrees)
+        public double deg { get; set; }
+    }
+    public class Clouds
+    {
+        //Cloudiness(%)
+        public int all { get; set; }
+    }
+    public class Sys
+    {
+        //Internal parameter 
+        public int type { get; set; }
+        //Internal parameter 
+        public int id { get; set; }
+        //Country Code
+        public string country { get; set; }
+        //Sunrise(unix,UTC)
+        public int sunrise { get; set; }
+        //Sunset(unix,UTC)
+        public int sunset { get; set; }
+    }
+    public class OpenWeatherMap 
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        //Internal parameter 
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        //Time of data calculation(unix, UTC)
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        //Shift in seconds from UTC
+        public int timezone { get; set; }
+        //Country ID
+        public int id { get; set; }
+        //Country Name
+        public string name { get; set; }
+        //Internal parameter 
+        public int cod { get; set; }
+    }
+}
---
+++ .NET_core/OpenWeatherMap_Test/OpenWeatherMap_Test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace OpenWeatherMap_Test
+{ 
+    class Program
+    {
+        /*
+           Information to receive may be changed by changing url
+           When changing, the class must be changed according to the information type.
+           Reference : https://openweathermap.org/api
+        */
+        static void Main()
+        {
+            //Your apiKey
+            string apiKey = "";
+            string city = "Daejeon";
+
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}",city, apiKey);
+
+            WebClient webClient = new WebClient();
+            string json = webClient.DownloadString(url);
+
+
+            OpenWeatherMap weatherInfo = (new JavaScriptSerializer()).Deserialize<OpenWeatherMap>(json);
+
+            Console.WriteLine("Location : ({0},{1})", weatherInfo.coord.lat, weatherInfo.coord.lon);
+
+            Console.WriteLine("\nWeather condition");
+            Console.WriteLine(" - ID : {0}\n - Main : {1}\n - Description : {2}\n - Icon : {3}",
+                weatherInfo.weather[0].id, weatherInfo.weather[0].main, weatherInfo.weather[0].description, weatherInfo.weather[0].icon);
+
+            Console.WriteLine("\nInternal parameter : {0}", weatherInfo.@base);
+
+            Console.WriteLine("\nTemperature");
+            Console.WriteLine(" - Temperature : {0}℃\n - Sensible Temperature : {1}K\n - Minimum Temperature : {2}℃\n - Maximum Temperature : {3}℃\n - Pressure : {4}hPa\n - Humidity : {5}%",
+             ConvertKelvin(weatherInfo.main.temp), weatherInfo.main.sensible_temp, ConvertKelvin(weatherInfo.main.temp_min), ConvertKelvin(weatherInfo.main.temp_max), weatherInfo.main.pressure, weatherInfo.main.humidity);
+
+            Console.WriteLine("\nWind");
+            Console.WriteLine(" - Speed : {0}m/s\n - Direction : {1} degrees", weatherInfo.wind.speed, weatherInfo.wind.deg);
+
+            Console.WriteLine("\nClouds");
+            Console.WriteLine(" - Cloudiness : {0}%", weatherInfo.clouds.all);
+
+            Console.WriteLine("\nSystem");
+            Console.WriteLine(" - Type : {0}\n - ID : {1}\n - Country : {2}\n - Sunrise : {3}\n - Sunset : {4}",
+                weatherInfo.sys.type, weatherInfo.sys.id, weatherInfo.sys.country, ConvertUnixtimeToGMT(weatherInfo.sys.sunrise).ToString("tt hh:mm:ss"), ConvertUnixtimeToGMT(weatherInfo.sys.sunset).ToString("tt hh:mm:ss"));
+
+            Console.WriteLine("\nEtc");
+            Console.WriteLine(" - Data receiving time : {0}\n - Shift UTC to Sec : {1} sec\n - City : {2}\n - Internal parameter : {3}",
+                weatherInfo.dt, weatherInfo.timezone, weatherInfo.name, weatherInfo.cod);
+            Console.WriteLine();
+        }
+
+        public static DateTime ConvertUnixtimeToGMT(int unixTime)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTime).ToLocalTime();
+
+            return dtDateTime; 
+        }
+        
+        public static double ConvertKelvin(double kelvin, bool convert = false)
+        {
+            if (convert)
+            {
+                return (kelvin - 273) * 5/9 + 32;
+            }
+
+            return kelvin - 273.15;
+        }
+    }
+}
