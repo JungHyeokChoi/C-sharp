2bd32195db209cf5a6a0765b527d17a1dbae2de9
JungHyeokChoi <page1114@gmail.com>
2020-07-24 15:10:03 +0900

    Test Ipapi

---
+++ .NET_core/Ipapi_test/Ipapi_test/IpInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Ipapi_test
+{
+    public class IpInfo
+    {
+        public string ip { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string region_code { get; set; }
+        public string country { get; set; }
+        public string country_name { get; set; }
+        public string postal { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string timezone { get; set; }
+        public string asn { get; set; }
+        public string org { get; set; }
+    }
+}
---
+++ .NET_core/Ipapi_test/Ipapi_test/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Ipapi_test
+{
+    class Program
+    {
+        static void  Main()
+        {
+            //Target IP Search : ipapi.co/{Target IP}/json
+            string url = string.Format("https://ipapi.co/json");
+
+            var t = Task<string>.Run(async () => {
+
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1");
+
+                string json = await httpClient.GetStringAsync(url);
+                
+                return json;
+            });
+
+            IpInfo ipinfo = JsonConvert.DeserializeObject<IpInfo>(t.Result);
+
+            Console.WriteLine("<IP Address Infomation>\n");
+            Console.WriteLine("IP : {0}", ipinfo.ip);
+            Console.WriteLine("City : {0}", ipinfo.city);
+            Console.WriteLine("Region : {0}", ipinfo.region);
+            Console.WriteLine("Region Code : {0}", ipinfo.region_code);
+            Console.WriteLine("Country : {0}", ipinfo.country);
+            Console.WriteLine("Country Name : {0}", ipinfo.country_name);
+            Console.WriteLine("Postal : {0}", ipinfo.postal);
+            Console.WriteLine("Location : ({0},{1})", ipinfo.latitude, ipinfo.longitude);
+            Console.WriteLine("Timezone : {0}", ipinfo.timezone);
+            Console.WriteLine("Asn : {0}", ipinfo.asn);
+            Console.WriteLine("Org : {0}", ipinfo.org);
+            Console.WriteLine();
+        }
+    }
+}
